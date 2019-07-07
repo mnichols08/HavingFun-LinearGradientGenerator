665e6baac9f86c2844af220fceb467f792efcf47
Mike Nichols <mikenichols410@gmail.com>
2019-07-06 20:55:07 -0400

    Made some extra changes, like bug catching

--- colorgen.js
+++ colorgen.js
@@ -2,12 +2,11 @@
     class Collection {
         constructor() {
             this.collection = []; // Creates an array on the parent object to store values created.
-            this.prev = this.collection.map(item => item);
+            this.prev = [];
             this.next = [];
         };
     }
     class Color extends Collection {
-
         constructor(degree, r, g, b, hue, sat, light) { // We are creating a linear gradient with Hue, Saturation, and Light
             super()
             this.direction = degree; // linear-gradient() degree variable
@@ -25,9 +24,11 @@
             document.body.style.background = style; // sets the body background to the generated hsl()
             document.querySelector('h2').innerText = `background: ${style}`; // passes the value into the h2 element so user can see it.
         }
-        addButton() {
-            document.querySelector('div:first-of-type #recall').classList.add('show')
-            document.querySelector('div:first-of-type #next').classList.add('show')
+        togglePrevButton() {
+           prev.length > 1 ? document.getElementById('recall').classList.add('show') : document.getElementById('recall').classList.remove('show')// applies show class to the recall button (referred to as prev throughout this lecture)
+        }
+        toggleNextButton() {
+            next.length > 1 ? document.getElementById('next').classList.add('show') : document.getElementById('next').classList.remove('show'); // applies show class to the next button - Needs more work to be satisfied with this.
         }
         rememberValues(values) {
             Collection.prototype.oldCollection = values.map(val => val);
@@ -45,63 +46,45 @@
                 randomL_t = randomRange(...happyMedium); // memory for now
             let thisColor = newColor(randomDeg, randomHue, randomSat, randomLgt, randomH_e, randomS_t, randomL_t); // finally creates the object using all the random numbers just set
             collection.push(thisColor); // pushes the values we created into storage on the parent to recall them later.
-            prev.push(thisColor);
+            prev.push(thisColor); // keeps history of previous backgrounds by pushing into another array
             const style = `linear-gradient(${randomDeg}deg, hsl(${randomHue}, ${randomSat}%, ${randomLgt}%),hsl(${randomH_e},${randomS_t}%,${randomL_t}%))` // writes style str
             thisColor.updateBody(style); // passes style string to the updateBody method on this newly created object.
-            collection.length > 1 ?  Color.prototype.addButton() : console.log('init'); // runs the addButton method on Color object
-            console.log('randomColor',collection);
-        }
+            Color.prototype.togglePrevButton(); // tells the previous button to appear on the screen
+        }       
         recallColor() {
-            if (prev.length >= 2 && prev.length !== 0) {
-                const lastColor = prev[prev.length - 2] // sets last color to the previous object in collection
-                const thisColor = newColor(lastColor.direction, lastColor.hue, lastColor.saturation, lastColor.light, lastColor.hue2, lastColor.sat, lastColor.lite); // creates a new object based on that
-                prev.pop(thisColor) // pops the values we recalled from storage on parent.
-                next.push(thisColor);
-                const style = `linear-gradient(${thisColor.direction}deg, hsl(${thisColor.hue}, ${thisColor.saturation}%, ${thisColor.light}%),hsl(${thisColor.hue2},${thisColor.sat}%,${thisColor.lite}%))` // writes style str
-                thisColor.updateBody(style); // passes style string to the updateBody method on this newly created object.
-                console.log('recallColor',prev,next);
-            }
+            Color.prototype.toggleNextButton(); // tells the next button to appear on screen 
+            try{
+                if (prev.length > 1) {
+                    const lastColor = prev[prev.length - 2] // sets last color to the previous object in collection
+                    const thisColor = newColor(lastColor.direction, lastColor.hue, lastColor.saturation, lastColor.light, lastColor.hue2, lastColor.sat, lastColor.lite); // creates a new object based on that
+                    prev.pop(thisColor) // pops the values we have seen off of the previous array
+                    next.push(thisColor); // sends them over to the next array so we can iterate through them ater
+                    const style = `linear-gradient(${thisColor.direction}deg, hsl(${thisColor.hue}, ${thisColor.saturation}%, ${thisColor.light}%),hsl(${thisColor.hue2},${thisColor.sat}%,${thisColor.lite}%))` // writes style str
+                    thisColor.updateBody(style); // passes style string to the updateBody method on this newly created object.
+                }
+            } catch(err) {console.error(`Caught issue: ${err}, running randomGenerator Anyway`, prev);Color.prototype.randomColor()} // Noticed some bugs if the array is too close to 0. This lets it keep appearing to work but reports the issue to console as an error
         }
-        nextColor() {        
-            if (next.length >= 2 && next.length !== 0) {
+        nextColor() {
+        try{
+            if (next.length > 1) {
                 const lastColor = next[next.length - 2] // sets last color to the previous object in collection
                 const thisColor = newColor(lastColor.direction, lastColor.hue, lastColor.saturation, lastColor.light, lastColor.hue2, lastColor.sat, lastColor.lite); // creates a new object based on that
-                next.pop(thisColor) // pops the values we recalled from storage on parent.
-                prev.push(thisColor);
+                next.pop(thisColor) // removes this object from the next as we move towards the end.
+                prev.push(thisColor); // maintains it's position by pushing the values back over to the previous array
                 const style = `linear-gradient(${thisColor.direction}deg, hsl(${thisColor.hue}, ${thisColor.saturation}%, ${thisColor.light}%),hsl(${thisColor.hue2},${thisColor.sat}%,${thisColor.lite}%))` // writes style str
                 thisColor.updateBody(style); // passes style string to the updateBody method on this newly created object.
-                console.log('randomColor',collection);
+                }
             }
-
+        catch(err){console.error(`Caught issue: ${err}, running randomGenerator Anyway`, next);Color.prototype.randomColor()} // Noticed some bugs if the array is too close to 0. This lets it keep appearing to work but reports the issue to console as an error
         }
     }
-
     const initCollection = new Collection() // assigns the collection variable from parent object to store colors
-    const collection = initCollection.collection;
-    let prev = initCollection.prev;
-    let next = initCollection.next;
+    const collection = initCollection.collection; // stores the initial collection into memory
+    let prev = initCollection.prev; // initializes the previous array into memory
+    let next = initCollection.next; // initializes the next array into memory
     const newColor = (deg, r, g, b, hue, sat, light) => new Color(deg, r, g, b, hue, sat, light); // sets up the Color Constructor
-
-    global.collection = collection; // passes the collection array to the global scope
-    global.prev = prev;
-    global.next = next;
 
     document.getElementById('change').addEventListener('click', new Color().randomColor); // listens for generate click on the button to update DOM on request.
     document.getElementById('recall').addEventListener('click', new Color().recallColor); // listens for recall click to update DOM on request
     document.getElementById('next').addEventListener('click', new Color().nextColor); // listens for recall click to update DOM on request
-})(window)
-
-
-
-// if (collection.length >= 2 && collection.length !== 0) {
-//     console.log(prev, collection)
-    
-//     const lastColor = collection[collection.length - 2] // sets last color to the previous object in collection
-//     const thisColor = newColor(lastColor.direction, lastColor.hue, lastColor.saturation, lastColor.light, lastColor.hue2, lastColor.sat, lastColor.lite); // creates a new object based on that
-//     // prev.of(thisColor) // pops the values we recalled from storage on parent.
-
-//     const style = `linear-gradient(${thisColor.direction}deg, hsl(${thisColor.hue}, ${thisColor.saturation}%, ${thisColor.light}%),hsl(${thisColor.hue2},${thisColor.sat}%,${thisColor.lite}%))` // writes style str
-//     thisColor.updateBody(style); // passes style string to the updateBody method on this newly created object.
-// } else if (collection.length === 1) {} else {
-//     Color.prototype.randomColor()
-// }; // If the parent has more than one color, proceed with recall, do nothing if 0 and return randomColor 
+})(window);
